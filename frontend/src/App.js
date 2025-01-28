6909b263f2d2507a0aee3934b8cfde86c3afbb50
Stalmax76 <68605477+Stalmax76@users.noreply.github.com>
2025-01-28 20:18:43 +0100

    first fetch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,12 +4,20 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Search from './components/Search';
 
+const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
+
 function App() {
   const [search, setSearch] = useState('');
   const handleSearchSubmit = (e) => {
     e.preventDefault();
     console.log(search);
+
+    fetch(`https://api.unsplash.com/photos/random/?query=${search}&client_id=${UNSPLASH_KEY}`)
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.log(err));
   };
+
   return (
     <div>
       <Header title='Images Gallary' />
