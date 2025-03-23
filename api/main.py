d226988303fd9339e1dff241f36d8cb38fc0a6e6
Stalmax76 <68605477+Stalmax76@users.noreply.github.com>
2025-03-23 20:00:42 +0100

    check request

--- api/main.py
+++ api/main.py
@@ -1,8 +1,34 @@
 # save this as app.py
-from flask import Flask
+import requests
+from flask import Flask, request
+
+UNSPLASH_URL ="https://api.unsplash.com/photos/random"
+UNSPLASH_KEY = "djsLUwDFseHW9eJkKLfYshTeLOlWRHHNsPNhltvKdts"
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello, World!"
+
+@app.route("/new-image")
+def new_image():
+    word = request.args.get("query")
+    headers= {
+        "Accept-Version": "v1",
+        "Authorization": f"Client-ID {UNSPLASH_KEY}"
+    }
+    params= {
+        "query":word
+    }
+    response = requests.get(url=UNSPLASH_URL, headers=headers, params=params)
+    data = response.json()
+    return data
+   
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
+# def hello():
+    # return "Hello, Vasyl!"
+
+
+
+
