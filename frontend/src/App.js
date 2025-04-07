17b722b4d4e126e777ce2172f7985d03fd85d327
Stalmax76 <68605477+Stalmax76@users.noreply.github.com>
2025-04-07 20:14:29 +0200

    delete image request from frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -41,9 +41,15 @@ function App() {
     }
     setSearch('');
   };
-  const handleDeleteImage = (id) => {
-    const newImages = images.filter((image) => image.id !== id);
-    setImages(newImages);
+  const handleDeleteImage = async (id) => {
+    try {
+      const res = await axios.delete(`${API_URL}/images/${id}`);
+      if (res.data?.deleted_id) {
+        setImages(images.filter((image) => image.id !== id));
+      }
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   const handelSaveImage = async (id) => {
