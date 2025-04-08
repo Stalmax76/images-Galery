d60f32c605cbc392a3423bbd68ae4e9aa9dd10a1
Stalmax76 <68605477+Stalmax76@users.noreply.github.com>
2025-04-08 19:22:41 +0200

    image author info

--- frontend/src/components/ImageCard.js
+++ frontend/src/components/ImageCard.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { Card, Button } from 'react-bootstrap';
+import Nav from 'react-bootstrap/Nav';
 
 const ImageCard = ({ image, deleteImage, saveImage }) => {
   const { urls, title, alt_description, description, id, saved } = image;
+  const authorName = image.user?.name || 'No author name';
+  const authorPortfolioURL = image.user?.portfolio_url;
 
   return (
     <Card style={{ width: '18rem', marginInline: 'auto', textAlign: 'center' }}>
@@ -21,6 +24,17 @@ const ImageCard = ({ image, deleteImage, saveImage }) => {
           </Button>
         )}
       </Card.Body>
+      <Card.Footer
+        className="text-muted text-center"
+        style={{ textTransform: 'capitalize' }}
+      >
+        {authorPortfolioURL && (
+          <Nav.Link href={authorPortfolioURL} target="_blank">
+            {authorName}
+          </Nav.Link>
+        )}
+        {!authorPortfolioURL && authorName}{' '}
+      </Card.Footer>
     </Card>
   );
 };
